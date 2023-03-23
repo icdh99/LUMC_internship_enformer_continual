f418ca40b052098200e3d342ff9d8b6bbb09f7c1
icdh99 <icdenhond@gmail.com>
2023-03-23 13:00:50 +0100

    move plots to new folders for correlation

--- correlation/analyse_tracks_correlation_dnn_head.py
+++ correlation/analyse_tracks_correlation_dnn_head.py
@@ -6,7 +6,7 @@ input_file = '/exports/humgen/idenhond/data/Basenji/human-targets.txt'
 df = pd.read_csv(input_file, sep = '\t')
 df[['assay type', 'description2']] = df.description.str.split(':', n = 1, expand = True) # make new column for assay type
 
-print(f'Number of tracks: {df.shape[0]}')
+print(f'Number of tracks: {df.shape[0]}\n')
 print(f"Number of trakcs per assay type: \n {df['assay type'].value_counts()}")
 
 # select 10 tracks with highest test correlation score
@@ -21,20 +21,30 @@ col_name = ['correlation validation']
 df_correlation_validation = pd.read_csv('//exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_valid_dnn_head.csv', index_col = 0, names = col_name)
 col_name = ['correlation valid enformer']
 df_correlation_validation_enformer = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_valid_own_output_pretrainedmodel.csv', index_col = 0, names = col_name)
+col_name = ['correlation train']
+df_correlation_train = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_train_dnn_head.csv', index_col = 0, names = col_name)
+col_name = ['correlation train enformer']
+df_correlation_train_enformer = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_train.csv', index_col = 0, names = col_name)
 
 df_correlation_test = df_correlation_test.tail(-1)
 df_correlation_test_enformer = df_correlation_test_enformer.tail(-1)
 df_correlation_validation = df_correlation_validation.tail(-1)
+df_correlation_validation_enformer = df_correlation_validation_enformer.tail(-1)
+df_correlation_train = df_correlation_train.tail(-1)
+df_correlation_train_enformer = df_correlation_train_enformer.tail(-1)
 
 # add column to df with test and validation correlation scores
 df['test correlation'] = df_correlation_test['correlation test']
 df['test correlation enformer'] = df_correlation_test_enformer['correlation test enformer']
 df['validation correlation'] = df_correlation_validation['correlation validation']
 df['validation correlation enformer'] = df_correlation_validation_enformer['correlation valid enformer']
+df['train correlation'] = df_correlation_train['correlation train']
+df['train correlation enformer'] = df_correlation_train_enformer['correlation train enformer']
 
 # calculate mean test and validation correlation score
 print(f'mean correlation score test dnn head: {df["test correlation"].mean(axis=0):.4f}')
 print(f'mean correlation score validation dnn head: {df["validation correlation"].mean(axis=0):.4f}')
+print(f'mean correlation score train dnn head: {df["train correlation"].mean(axis=0):.4f}')
 
 plt.figure(1)
 sns.boxplot(data = df[['test correlation', 'test correlation enformer']], showmeans = True)
@@ -47,31 +57,51 @@ plt.ylabel('Pearson Correlation Coefficient')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_boxplot_valid_vsenformer_corr.png', bbox_inches='tight')
 
 plt.figure(3)
+sns.boxplot(data = df[['train correlation', 'train correlation enformer']], showmeans = True)
+plt.ylabel('Pearson Correlation Coefficient')
+plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_boxplot_train_vsenformer_corr.png', bbox_inches='tight')
+
+plt.figure(4)
 sns.boxplot(data = df[['test correlation', 'validation correlation']], showmeans = True)
 plt.ylabel('Pearson Correlation Coefficient')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_boxplot_test_valid_corr.png', bbox_inches='tight')
 
-plt.figure(4)
+plt.figure(5)
+sns.boxplot(data = df[['train correlation', 'test correlation']], showmeans = True)
+plt.ylabel('Pearson Correlation Coefficient')
+plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_boxplot_test_train_corr.png', bbox_inches='tight')
+
+plt.figure(6)
 plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
 sns.scatterplot(data = df, x = 'test correlation enformer', y = 'test correlation')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_test_enformer_corr.png', bbox_inches='tight')
 
-plt.figure(5)
+plt.figure(7)
 plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
 sns.scatterplot(data = df, x = 'test correlation enformer', y = 'test correlation', hue = 'assay type')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_test_enformer_corr_asssaytype.png', bbox_inches='tight')
 
-plt.figure(6)
+plt.figure(8)
 plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
 sns.scatterplot(data = df, x = 'validation correlation enformer', y = 'validation correlation')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_valid_enformer_corr.png', bbox_inches='tight')
 
-plt.figure(7)
+plt.figure(9)
 plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
 sns.scatterplot(data = df, x = 'validation correlation enformer', y = 'validation correlation', hue = 'assay type')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_valid_enformer_corr_asssaytype.png', bbox_inches='tight')
 
-plt.figure(8)
+plt.figure(10)
+plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
+sns.scatterplot(data = df, x = 'train correlation enformer', y = 'train correlation')
+plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_train_enformer_corr.png', bbox_inches='tight')
+
+plt.figure(11)
+plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
+sns.scatterplot(data = df, x = 'train correlation enformer', y = 'train correlation', hue = 'assay type')
+plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_train_enformer_corr_asssaytype.png', bbox_inches='tight')
+
+plt.figure(12)
 plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
 sns.scatterplot(data = df, x = 'test correlation', y = 'validation correlation', hue = 'assay type')
 plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_scatterplot_valid_test_corr_asssaytype.png', bbox_inches='tight')
@@ -101,3 +131,11 @@ for key, value in df['assay type'].value_counts().to_dict().items():
     plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
     sns.scatterplot(data = df_subset, x = 'validation correlation enformer', y = 'validation correlation')
     plt.savefig(f'/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_pretrainedmodel_scatterplot_valid_enformer_corr_{key}.png', bbox_inches='tight')
+
+for key, value in df['assay type'].value_counts().to_dict().items():
+    df_subset = df[df['assay type'] == key]
+    plt.figure()
+    plt.title(f'Assay type: {key}')
+    plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
+    sns.scatterplot(data = df_subset, x = 'train correlation enformer', y = 'train correlation')
+    plt.savefig(f'/exports/humgen/idenhond/projects/enformer/correlation/Plots/dnn_head/dnn_head_pretrainedmodel_scatterplot_train_enformer_corr_{key}.png', bbox_inches='tight')
--- correlation/analyse_tracks_correlation_perassaytype_ownoutputpretrainedmodel.py
+++ correlation/analyse_tracks_correlation_perassaytype_ownoutputpretrainedmodel.py
@@ -8,7 +8,6 @@ input_file = '/exports/humgen/idenhond/data/Basenji/human-targets.txt'
 df = pd.read_csv(input_file, sep = '\t')
 df[['assay type', 'description2']] = df.description.str.split(':', n = 1, expand = True) # make new column for assay type
 
-
 def f(row):
     if row['assay type'] == 'CHIP':
         if any(row['description2'].startswith(x) for x in ['H2AK', 'H2BK', 'H3K', 'H4K']):
@@ -23,82 +22,81 @@ def f(row):
 
 df['assay type split ChIP'] = df.apply(f, axis=1)
 
-
-
 print(f'Number of tracks: {df.shape[0]}')
 # print(f"Number of trakcs per assay type: \n {df['assay type'].value_counts()}")
 print(f"Number of trakcs per assay type split: \n {df['assay type split ChIP'].value_counts()}")
 
-
-
-
-# select 10 tracks with highest test correlation score
-    # TODO ..... 
-
-# read csv with correlation score per track for test and validation sequences
+# read csv with correlation score per track for test and validation and train sequences
 col_name = ['correlation test']
-df_correlation_test = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_test_own_output_newmodel.csv', index_col = 0, names = col_name)
+df_correlation_test = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_test_own_output_pretrainedmodel.csv', index_col = 0, names = col_name)
 col_name = ['correlation validation']
-df_correlation_validation = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_valid_own_output_newmodel.csv', index_col = 0, names = col_name)
-
+df_correlation_validation = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_valid_own_output_pretrainedmodel.csv', index_col = 0, names = col_name)
+col_name = ['correlation train']
+df_correlation_train = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_train.csv', index_col = 0, names = col_name)
 df_correlation_test = df_correlation_test.tail(-1)
 df_correlation_validation = df_correlation_validation.tail(-1)
+df_correlation_train = df_correlation_train.tail(-1)
 
 # add column to df with test and validation correlation scores
 df['test correlation'] = df_correlation_test['correlation test']
+
 df['validation correlation'] = df_correlation_validation['correlation validation']
+df['train correlation'] = df_correlation_train['correlation train']
+print(df)
+# calculate mean test and validation correlation score
+# print(f'mean correlation score test: {df["test correlation"].mean(axis=0):.4f}')
+# print(f'mean correlation score validation: {df["validation correlation"].mean(axis=0):.4f}')
+# print(f'mean correlation score train: {df["train correlation"].mean(axis=0):.4f}')
+
+# print('\nmean correlation score test per assay type: ')
+# for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
+#     df_subset = df[df['assay type split ChIP'] == key]
+#     print(f'mean correlation score test {key}: {df_subset["test correlation"].mean(axis=0):.4f}')
+
+# print('\nmean correlation score valid per assay type: ')
+# for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
+#     df_subset = df[df['assay type split ChIP'] == key]
+#     print(f'mean correlation score valid {key}: {df_subset["validation correlation"].mean(axis=0):.4f}')
+
+
+# print('\nmean correlation score train per assay type: ')
+# for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
+#     df_subset = df[df['assay type split ChIP'] == key]
+#     print(f'mean correlation score train {key}: {df_subset["train correlation"].mean(axis=0):.4f}')
+
+# read csv with correlation score per track for test and validation and train sequences dnn head
+col_name = ['correlation test dnn head']
+df_correlation_test_dnn_head = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_test_dnn_head.csv', index_col = 0, names = col_name)
+col_name = ['correlation validation dnn head']
+df_correlation_validation_dnn_head = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_valid_dnn_head.csv', index_col = 0, names = col_name)
+col_name = ['correlation train dnn head']
+df_correlation_train_dnn_head = pd.read_csv('/exports/humgen/idenhond/data/evaluate_correlation/correlation_per_track_train_dnn_head.csv', index_col = 0, names = col_name)
+
+df_correlation_test_dnn_head = df_correlation_test_dnn_head.tail(-1)
+df_correlation_validation_dnn_head = df_correlation_validation_dnn_head.tail(-1)
+df_correlation_train_dnn_head = df_correlation_train_dnn_head.tail(-1)
+# add column to df with test and validation correlation scores
+
+df['test correlation dnn head'] = df_correlation_test_dnn_head['correlation test dnn head']
+df['validation correlation dnn head'] = df_correlation_validation_dnn_head['correlation validation dnn head']
+df['train correlation dnn head'] = df_correlation_train_dnn_head['correlation train dnn head']
 
 # calculate mean test and validation correlation score
-print(f'mean correlation score test: {df["test correlation"].mean(axis=0):.4f}')
-print(f'mean correlation score validation: {df["validation correlation"].mean(axis=0):.4f}')
+print(f'mean correlation score test dnn head: {df["test correlation dnn head"].mean(axis=0):.4f}')
+print(f'mean correlation score validation dnn head: {df["validation correlation dnn head"].mean(axis=0):.4f}')
+print(f'mean correlation score train dnn head: {df["train correlation dnn head"].mean(axis=0):.4f}')
 
-print('\nmean correlation score test per assay type: ')
+print('\nmean correlation score test per assay type dnn head: ')
 for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
     df_subset = df[df['assay type split ChIP'] == key]
-    print(f'mean correlation score test {key}: {df_subset["test correlation"].mean(axis=0):.4f}')
+    print(f'mean correlation score test {key}: {df_subset["test correlation dnn head"].mean(axis=0):.4f}')
 
+print('\nmean correlation score valid per assay type dnn head: ')
+for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
+    df_subset = df[df['assay type split ChIP'] == key]
+    print(f'mean correlation score valid {key}: {df_subset["validation correlation dnn head"].mean(axis=0):.4f}')
 
-
-
-
-
-exit()
-
-
-
-
-
-
-exit()
-# plot boxplot with test and boxplot with validation correlation score
-    # center line indicates median correlation score 
-    # showmeans=True
-plt.figure(1)
-sns.boxplot(data = df[['test correlation', 'validation correlation']], showmeans = True)
-    # boxplot for multiple numerical columns
-plt.ylabel('Pearson Correlation Coefficient')
-plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/own_output_newmodel_boxplot_test_val_corr.png', bbox_inches='tight')
-
-plt.figure(2)
-plt.ylabel('Pearson Correlation Coefficient')
-sns.violinplot(data = df[['test correlation', 'validation correlation']], orient = 'v')
-plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/own_output_newmodel_violinplot_test_val_corr.png', bbox_inches='tight')
-
-# plot scatterplot with validation on x and test on y axis (5313 points) for all points and per assay type
-plt.figure(3)
-plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
-sns.scatterplot(data = df, x = 'test correlation', y = 'validation correlation')
-plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/own_output_newmodel_scatterplot_test_val_corr.png', bbox_inches='tight')
-
-plt.figure(4)
-plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
-sns.scatterplot(data = df, x = 'test correlation', y = 'validation correlation', hue = 'assay type')
-plt.savefig('/exports/humgen/idenhond/projects/enformer/correlation/Plots/own_output_newmodel_scatterplot_test_val_corr_asssaytype.png', bbox_inches='tight')
-
-for key, value in df['assay type'].value_counts().to_dict().items():
-    df_subset = df[df['assay type'] == key]
-    plt.figure()
-    plt.title(f'Assay type: {key}')
-    plt.axline((0, 0), (1, 1), linewidth=0.5, color='k', linestyle = 'dashed')
-    sns.scatterplot(data = df_subset, x = 'test correlation', y = 'validation correlation')
-    plt.savefig(f'/exports/humgen/idenhond/projects/enformer/correlation/Plots/own_output_newmodel_scatterplot_test_val_corr_{key}.png', bbox_inches='tight')
+print('\nmean correlation score train per assay type train head: ')
+for key, value in df['assay type split ChIP'].value_counts().to_dict().items():
+    df_subset = df[df['assay type split ChIP'] == key]
+    print(f'mean correlation score test {key}: {df_subset["train correlation dnn head"].mean(axis=0):.4f}')
