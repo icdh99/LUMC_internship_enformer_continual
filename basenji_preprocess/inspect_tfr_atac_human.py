2794dfd211ef7e082d2300c67010287c98848fc9
icdh99 <icdenhond@gmail.com>
2023-04-24 10:12:33 +0200

    plot human atac tfr files

---
+++ basenji_preprocess/inspect_tfr_atac_human.py
@@ -0,0 +1,64 @@
+import os
+import tensorflow as tf
+from natsort import natsorted
+import glob
+import matplotlib.pyplot as plt
+
+print(f'\nrunning inspect_tfr.py')
+
+NUM_TRACKS = 66
+
+def make_parser(): #, rna_mode
+    def parse_proto(example_protos):
+        """Parse TFRecord protobuf."""
+
+        feature_spec = {
+        'sequence': tf.io.FixedLenFeature([], dtype = tf.string),  # Ignore this, resize our own bigger one
+        'target': tf.io.FixedLenFeature([], dtype = tf.string),
+        }
+
+        # parse example into features
+        feature_tensors = tf.io.parse_single_example(example_protos, features=feature_spec)
+
+        sequence = tf.io.decode_raw(feature_tensors['sequence'], tf.bool)
+        sequence = tf.reshape(sequence, (131072, 4))
+        sequence = tf.cast(sequence, tf.float32)
+
+        target = tf.io.decode_raw(feature_tensors['target'], tf.float16)
+        target = tf.reshape(target, (896, NUM_TRACKS))
+        target = tf.cast(target, tf.float32)
+
+        return target, sequence
+
+    return parse_proto
+
+def file_to_records(filename):
+    return tf.data.TFRecordDataset(filename, compression_type='ZLIB')
+
+
+def get_target():
+    tfr_path = f'/exports/humgen/idenhond/data/basenji_preprocess/output_tfr_human_atac_scale1/tfrecords/train-0.tfr'
+    tfr_files = natsorted(glob.glob(tfr_path))
+    print(f'number of tfr files: {len(tfr_files)}')
+    print(tfr_files)
+    dataset = tf.data.Dataset.from_tensor_slices(tfr_files)
+    dataset = dataset.flat_map(file_to_records)
+    dataset = dataset.map(make_parser())
+    dataset = dataset.batch(1)
+    print(type(dataset))
+
+    for i, (target, sequence) in enumerate(dataset):
+        print(i, target.shape)
+        target = tf.squeeze(target)
+        print(target[:, 0]) # remove first dimension to get shape (896, 1)
+        plt.plot(target[:, 1], label = 'track 0')
+        plt.savefig(f'plots_human_atac/humanatac-train_seq{i+1}_track2.png')
+        plt.close()
+        if i == 1:
+            break
+    return None
+
+get_target()
+
+
+
