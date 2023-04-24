542684228fde1d77f4ea03dd37e30de2a474e7d6
icdh99 <icdenhond@gmail.com>
2023-04-24 18:14:38 +0200

    train model on all trakcs 2404

---
+++ enformer/dnn_head/train_alltracks_2404/README.md
@@ -0,0 +1,14 @@
+This folder contains the scripts necessary to train a DNN head model on all tracks (5313 enformer tracks, 27 new tracks()
+
+This model uses the 27 new tracks generated at 24/4 and the 5313 enformer tracks. 
+
+*Input data*
+Train input: /exports/humgen/idenhond/data/Enformer_train/Enformer_train_embeddings_pretrainedmodel
+Train output: /exports/humgen/idenhond/data/Enformer_train/Enformer_train_targets, /exports/humgen/idenhond/data/Enformer_train/Newtracks_2404_train_targets
+Validation input: /exports/humgen/idenhond/data/Enformer_validation/Enformer_validation_embeddings_pretrainedmodel_perseq
+Validation output: /exports/humgen/idenhond/data/Enformer_validation/Enformer_validation_targets_perseq, /exports/humgen/idenhond/data/Enformer_validation/Newtracks_2404_validation_targets
+
+
+
+
+
---
+++ enformer/dnn_head/train_alltracks_2404/data_class_withval.py
@@ -0,0 +1,44 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import random
+
+"""
+https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel
+
+partition: {'train': ['id-1', 'id-2', 'id-3'], 'validation': ['id-4']}
+list of training/validation IDs --> zelf maken
+
+"""
+class MyDataset_train(Dataset):
+    def __init__(self, list_IDs):
+        assert type(list_IDs) == list, f'list IDs expected type list but got {type(list_IDs)}'
+        self.list_IDs = list_IDs
+
+    def __len__(self):
+        return len(self.list_IDs)
+    
+    def __getitem__(self, index):
+        ID = self.list_IDs[index]
+        # load data and get label
+        x = torch.load(f'/exports/humgen/idenhond/data/Enformer_train/Enformer_train_embeddings_pretrainedmodel/embeddings_seq{ID}.pt', map_location=torch.device('cpu')) 
+        y_oldtracks = torch.load(f'/exports/humgen/idenhond/data/Enformer_train/Enformer_train_targets/targets_seq{ID}.pt', map_location=torch.device('cpu')).squeeze()
+        y_newtracks = torch.load(f'/exports/humgen/idenhond/data/Enformer_train/Newtracks_2404_train_targets/targets_seq{ID}.pt', map_location=torch.device('cpu')).squeeze() #TODO
+        y = torch.cat((y_oldtracks, y_newtracks), dim = 1)
+        return x, y
+
+class MyDataset_val(Dataset):
+    def __init__(self, list_IDs):
+        assert type(list_IDs) == list, f'list IDs expected type list but got {type(list_IDs)}'
+        self.list_IDs = list_IDs
+
+    def __len__(self):
+        return len(self.list_IDs)
+    
+    def __getitem__(self, index):
+        ID = self.list_IDs[index]
+        # load data and get label
+        x = torch.load(f'/exports/humgen/idenhond/data/Enformer_validation/Enformer_validation_embeddings_pretrainedmodel_perseq/embeddings_seq{ID}.pt', map_location=torch.device('cpu')) 
+        y_oldtracks = torch.load(f'/exports/humgen/idenhond/data/Enformer_validation/Enformer_validation_targets_perseq/targets_seq{ID}.pt', map_location=torch.device('cpu')).squeeze()
+        y_newtracks = torch.load(f'/exports/humgen/idenhond/data/Enformer_validation/Newtracks_2404_validation_targets/targets_seq{ID}.pt', map_location=torch.device('cpu')).squeeze() #TODO
+        y = torch.cat((y_oldtracks, y_newtracks), dim = 1)
+        return x, y
