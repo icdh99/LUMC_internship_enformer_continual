88f0c80b0335bb86c94fce2b090dcd462686259b
icdh99 <icdenhond@gmail.com>
2023-04-24 15:47:33 +0200

    train model on human atac tracks

---
+++ enformer/dnn_head/train_human_atac/README.md
@@ -0,0 +1,16 @@
+This folder contains the scripts necessary to train a DNN head model on the human ATAC-seq tracks from the paper 'A transcriptomic and epigenomic cell atlas of the mouse primary motor cortex'.
+
+Raw bigwig files are derived from: https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=1609681239_fUFy3yi8hM5baXlnKyZFAvUuEyR5&c=chr15&g=hub_2136863_humanMopATAC%2DseqMultiwigs
+-- 66 tracks
+
+The script /exports/humgen/idenhond/projects/basenji_preprocess/make_tfr_atac_human.sh is used to create the tensorflow records (tfr).
+TFRs are stored at /exports/humgen/idenhond/data/basenji_preprocess/output_tfr_human_atac/tfrecords
+Target information is stored at /exports/humgen/idenhond/data/basenji_preprocess/output_tfr_human_atac/targets.txt
+
+Example plots comparing the tensorflow records and the bw tracks in IGV are stored at /exports/humgen/idenhond/projects/basenji_preprocess/plots_human_atac
+
+For my model training pipeline, the targets need to be stored in .pt format. 
+-- /exports/humgen/idenhond/projects/enformer/dnn_head/train_human_atac/store_target_as_tensor.sh
+
+
+
---
+++ enformer/dnn_head/train_human_atac/store_target_as_tensor.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+start = datetime.now()
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from collections import OrderedDict
+from sklearn.model_selection import train_test_split
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning.callbacks import RichProgressBar
+from natsort import natsorted
+import glob
+import tensorflow as tf
+import sys
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device (set to GPU if available):', device)
+
+subset = sys.argv[1]
+print(f'subset: {subset}')
+
+def make_parser(): #, rna_mode
+    def parse_proto(example_protos):
+        """Parse TFRecord protobuf."""
+        feature_spec = {
+        'sequence': tf.io.FixedLenFeature([], dtype = tf.string),
+        'target': tf.io.FixedLenFeature([], dtype = tf.string),
+        }
+        feature_tensors = tf.io.parse_single_example(example_protos, features=feature_spec)
+        target = tf.io.decode_raw(feature_tensors['target'], tf.float16)
+        target = tf.reshape(target, (896, 5313))
+        target = tf.cast(target, tf.float32)
+        return target
+    return parse_proto
+
+def file_to_records(filename):
+    return tf.data.TFRecordDataset(filename, compression_type='ZLIB')
+
+def get_target(subset = subset): 
+    tfr_path = f'/exports/humgen/idenhond/data/basenji_preprocess/output_tfr_human_atac/tfrecords/{subset}*.tfr'
+    tfr_files = natsorted(glob.glob(tfr_path))
+    print(f'number of tfr files for {subset} subset: {len(tfr_files)}')
+    dataset = tf.data.Dataset.from_tensor_slices(tfr_files)
+    dataset = dataset.flat_map(file_to_records)
+    dataset = dataset.map(make_parser())
+    dataset = dataset.batch(1)
+
+    # instead of adding targets to big array, save to seperate file in iteration
+    for i, target in enumerate(dataset):
+        target_tensor = torch.from_numpy(target.numpy())
+        torch.save(target_tensor, f'/exports/humgen/idenhond/data/Enformer_{subset}/Human_ATAC_{subset}_targets/targets_seq{i+1}.pt')
+    return None
+
+get_target()
